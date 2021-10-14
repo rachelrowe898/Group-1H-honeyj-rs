b7b098aec5ce72e5cf80aae1703eec56f300eebd
jlee882 <87512552+jlee882@users.noreply.github.com>
2021-10-14 16:10:53 -0400

    Create install_services.sh

---
+++ install_services.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# installs the following services specified to a started container
+#
+# wget
+# curl
+# ssh
+
+if [ $# -ne 1 ]
+then
+  echo "usage: $0 <container_name>"
+  exit 1
+fi
+
+# assumes container exist
+sudo lxc-attach -n $1 -- apt-get install wget -y
+sudo lxc-attach -n $1 -- apt-get install curl -y
+
+sudo lxc-attach -n $1 -- apt-get install openssh-server -y
