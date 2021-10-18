ce18065e4269dcd955b0b300c0b8547896f06841
stcwang <73036278+stcwang@users.noreply.github.com>
2021-10-18 16:35:00 -0400

    Template creation script (#12)
    * start template collection script
    
    * finish template creation script
    
    * fix typo
    
    * remove commented line
    
    * add download for lxc-create
    
    * add comments + two changes
    
    * motd message edit
    
    * minor readme edit
    
    * Update README.md
    
    * add other users

--- README.md
+++ README.md
@@ -2,11 +2,19 @@
 
 ## Honey Creation
 
-To run the honey creation script, execute the following command, replacing 10 with the number of lines of data you want. 
+To run the honey creation script, execute the following command. 
 ```bash
-python3 honey-creation.py 10
+python3 honey-creation.py <number of lines>
 ```
-The script will create (or replace, if the file already exists) an `employee-data.csv` file with a heading line and the given number of lines of data. An `employee-data.csv` file with 10 lines is provided as a reference. 
+The script will create (or replace, if the file already exists) an `employee-data.csv` file with a heading line and the given number of lines of data. 
+
+## Template Creation
+
+To run the template creation script, execute the following command, with either A, B, C, or D as an argument (alternatively, you can change permissions through `sudo chmod a+x template-creation.sh` and run without the `bash` keyword). 
+```bash
+bash template-creation.sh <template letter>
+```
+The four letters correspond to which number of lines there are in the honey file: A to 0, B to 5000, C to 50000, and D to 500000. An lxc container of the name letter_template will be created. Ideally, this script should only be run once per template, as we are not replacing the templates unless necessary. 
 
 ## Symlink Creation
 
---
+++ template-creation.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "usage: $0 <template identifier>"
+  exit 1
+fi
+
+lines=0
+
+if [ "$1" == 'A' ]; then
+  lines=0
+elif [ $"$1" == 'B' ]; then
+  lines=5000
+elif [ "$1" == 'C' ]; then
+  lines=50000
+elif [ "$1" == 'D' ]; then
+  lines=500000
+else
+  echo "invalid template identifier (must be A, B, C, or D)"
+  exit 2
+fi
+
+name="$1_template"
+
+sudo DOWNLOAD_KEYSERVER="keyserver.ubuntu.com" lxc-create -n "$name" -t download -- -d ubuntu -r focal -a amd64
+sudo lxc-start -n "$name"
+
+p="/var/lib/lxc/$name/rootfs"
+
+# this is for making the shared folder
+sudo lxc-attach -n "$name" -- mkdir shared
+python3 honey-creation.py $lines
+sudo mv employee-data.csv "$p/shared"
+
+# adding fake users + symlinks
+sudo lxc-attach -n "$name" -- ln -s "/shared/" "/home/ubuntu"
+for user in admin hbrown orange rli194 peterjp aj14 mrobinson kels dranna william bkrds umichaels rye panr ohara ellapatterson cnjr mrsamson kailee florence; do
+  pass=$(echo $RANDOM | base64 | head -c 20; echo)
+  echo -e "$pass\n$pass" | sudo lxc-attach -n "$name" -- adduser "$user" 
+  sudo lxc-attach -n "$name" -- ln -s "/shared/" "/home/$user"
+done 
+
+# this is for updating the ssh banner
+sudo lxc-attach -n "$name" -- apt-get update
+sudo lxc-attach -n "$name" -- apt-get install openssh-server -y
+
+for file in $(sudo ls "$p/etc/update-motd.d/"); do 
+  sudo chmod a-x "$p/etc/update-motd.d/$file"
+done 
+
+motd="01-company"
+
+sudo cp "$motd" "$p/etc/update-motd.d/"
+sudo chmod u+x "$p/etc/update-motd.d/$motd"
+
+sudo lxc-stop -n "$name"
