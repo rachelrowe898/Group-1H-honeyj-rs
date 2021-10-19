3163288865936610a03c1298a076c219929fc9e0
jlee882 <87512552+jlee882@users.noreply.github.com>
2021-10-19 15:57:14 -0400

    Datascripts (#16)
    * Create data_compression.sh
    
    * Create install_services.sh
    
    * Create poison_commands.sh
    
    * Update poison commands, downloaded file naming
    
    * Finished malware_monitoring.sh

---
+++ data_compression.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# compresses all data files in a directory for a honeypot attack
+# and moves these compressed into another directory
+# else where
+
+
+if [ $# -ne 4 ]
+then
+  echo "Usage: $0
+      <file path to directory with />
+      <compressed files dest path with />
+      <honeypot code>
+      <attackerID>"
+  exit 1
+fi
+
+if [ -d "$1" ]
+then
+  destdir="$2$3_$4$(date +_%m_%d)" #Zipped directory naming: [containerID]_[attackerID]_MM_DD.zip
+  zip -r "$destdir" "$1"
+fi
