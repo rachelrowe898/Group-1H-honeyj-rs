c8649dc293eb8f33e41ca0a9dac32be30a8d0fb8
yieb <52669318+yieb@users.noreply.github.com>
2021-10-21 18:36:10 -0400

    Update monitor-new-users.sh

--- monitor-new-users.sh
+++ monitor-new-users.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+### Unused currently
 
 if [ $# -lt 1 ]; then
-    echo "usage: $0 <container name>"
-    exit 1
+  echo "usage: $0 <container name>"
+  exit 1
 fi
 
 p="/var/lib/lxc/$1/rootfs"
 
 sudo inotifywait -m "$p/home" -e create |
-    while read dir action file; do
-        # echo "The file '$file' appeared in directory '$dir' via '$action'"
-        if [ "$action" = "CREATE,ISDIR" ]; then
-            sudo lxc-attach -n "$1" -- ln -s "/shared/" "/home/$file"
-        fi
-    done
+  while read dir action file; do
+    # echo "The file '$file' appeared in directory '$dir' via '$action'"
+    if [ "$action" = "CREATE,ISDIR" ]; then
+      sudo lxc-attach -n "$1" -- ln -s "/shared/" "/home/$file"
+    fi
+  done
