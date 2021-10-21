3234a5cf09eb87b38f91044642314e7f8f630b08
yieb <52669318+yieb@users.noreply.github.com>
2021-10-21 18:27:38 -0400

    Update recycle_honeypot.sh

--- recycle_honeypot.sh
+++ recycle_honeypot.sh
@@ -7,12 +7,6 @@
 # NOTE: Template containers should all end in "[name]_template" and the 
 # created honeypot will be called "[name]"
 
-
-# change back MITM config file after working on it 
-# note: script to poison the wget and curl commands will 
-# already be present in template containers
-# note: Copying off malware from container to sandbox VM will be separate script
-
 # First check if proper number of shell arguments is given
 if [ $# -ne 5 ]; then
   echo "usage: $0 <container_to_recycle> <external_IP>\
@@ -27,7 +21,8 @@ netmask_prefix=$3
 mitm_port=$4
 mitm_log_path=$5
 
-mitm_path="/home/eric/MITM/mitm/index.js"
+# Manually specify MITM path
+mitm_path="/home/student/Group-1H-honeyj-rs/MITM/mitm/index.js"
 
 # ensure UMD network interface is up
 sudo ip link set dev enp4s2 up
@@ -41,7 +36,7 @@ else
   echo "Recycling existing honeypot ${honeypot}..."
 # Else, recycle honeypot
   sudo bash delete_honeypot.sh "$honeypot" "$template" \
-    "$external_ip" "$netmask_prefix" "$mitm_port" "$mitm_path" "$mitm_log_path"
+    "$external_ip" "$netmask_prefix" "$mitm_port" "$mitm_path"
   sudo bash create_honeypot.sh "$honeypot" "$template" \
     "$external_ip" "$netmask_prefix" "$mitm_port" "$mitm_path" "$mitm_log_path"
 fi
