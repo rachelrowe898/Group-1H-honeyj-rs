163178f7de61d03bba5f94e5bee2d77ddb4390a7
yieb <52669318+yieb@users.noreply.github.com>
2021-10-21 21:15:31 -0400

    Create recycle_all_honeypots.sh

---
+++ recycle_all_honeypots.sh
@@ -0,0 +1,6 @@
+# Run script on restart using crontab
+
+sudo bash recycle_honeypot.sh HRServeA 128.8.238.119 26 10000 /home/student/active_data_A
+sudo bash recycle_honeypot.sh HRServeB 128.8.238.86 26 10001 /home/student/active_data_B
+sudo bash recycle_honeypot.sh HRServeC 128.8.238.120 26 10002 /home/student/active_data_C
+sudo bash recycle_honeypot.sh HRServeD 128.8.37.125 27 10003 /home/student/active_data_D
