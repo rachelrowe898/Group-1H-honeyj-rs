716bce2e3d6e94f7431f28bc1558842cd7537428
jlee882 <87512552+jlee882@users.noreply.github.com>
2021-10-22 01:09:19 -0400

    Update delete_honeypot.sh

--- delete_honeypot.sh
+++ delete_honeypot.sh
@@ -63,8 +63,12 @@ sudo ip addr delete "${external_ip}/${netmask_prefix}" dev enp4s2
 
 # Stop data collection background processes
 container_code=${honeypot: -1}
-dataJobNum=$(ps aux | grep "malware_monitoring.sh" | grep "HRServe$container_code" | awk '{ print $2 }')
-sudo kill -9 %${dataJobNum}
+dataJobNum=$(ps aux | grep "malware_monitoring.sh" | grep "HRServe$container_code" | awk '{ print $2 }' | sed -n 1p)
+
+while [ $dataJobNum != "" ] ; do
+  sudo kill -9 %${dataJobNum}
+  dataJobNum=$(ps aux | grep "malware_monitoring.sh" | grep "HRServe$container_code" | awk '{ print $2 }' | sed -n 1p)
+done
 echo "Malware monitoring stopped"
 
 
