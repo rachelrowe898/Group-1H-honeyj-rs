23ca5dd0f27e6720c72317785e4c8ee64e309744
yieb <52669318+yieb@users.noreply.github.com>
2021-10-21 21:52:49 -0400

    Update recycle_honeypot_aux.sh

--- recycle_honeypot_aux.sh
+++ recycle_honeypot_aux.sh
@@ -11,13 +11,13 @@ fi
 honeypot=$(echo $1 | sed 's/HRServe//')
 
 if [ "$honeypot" == "A" ]; then
-  sudo bash recycle_honeypot.sh A 128.8.238.119 26 10000 /home/student/active_data_A
+  sudo bash recycle_honeypot.sh HRServeA 128.8.238.119 26 10000 /home/student/active_data_A
 elif [ "$honeypot" == "B" ]; then 
-  sudo bash recycle_honeypot.sh B 128.8.238.86 26 10001 /home/student/active_data_B
+  sudo bash recycle_honeypot.sh HRServeB 128.8.238.86 26 10001 /home/student/active_data_B
 elif [ "$honeypot" == "C" ]; then 
-  sudo bash recycle_honeypot.sh C 128.8.238.120 26 10002 /home/student/active_data_C
+  sudo bash recycle_honeypot.sh HRServeC 128.8.238.120 26 10002 /home/student/active_data_C
 elif [ "$honeypot" == "D" ]; then 
-  sudo bash recycle_honeypot.sh D 128.8.37.125 27 10003 /home/student/active_data_D
+  sudo bash recycle_honeypot.sh HRServeD 128.8.37.125 27 10003 /home/student/active_data_D
 else
   echo "Invalid honeypot name. Cannot recycle honeypot."
   exit 1
