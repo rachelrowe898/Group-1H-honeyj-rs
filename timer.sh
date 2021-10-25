2203612a7b10499e2eae49a6049ff270fbcfaabc
stcwang <73036278+stcwang@users.noreply.github.com>
2021-10-25 16:58:44 -0400

    Update mitm monitor (#38)
    * add iprule check & 3hr timer
    
    * remove test file
    
    * uncomment line
    
    * remove testing stuff

---
+++ timer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# -ne 4 ]; then
+  echo "usage: $0 <mitm log file path> <attacker_ip> <host_ip> <mitm port>"
+  exit 1
+fi
+
+echo "TIMER RUNNING"
+container=$(echo "$1" | cut -d '/' -f 5 | sed 's/.log//')
+attacker_ip="$2"
+host_ip="$3"
+mitm_port="$4"
+
+sleep 10800
+
+# murder monitoring script
+ps -aux | grep "monitor-mitm.sh $1" |  awk '{ print $2 }' | sed '$ d' | xargs kill
+
+echo "REMOVING (sleep)"
+
+# end stuff
+sudo iptables -D INPUT -s "$attacker_ip" -d "$host_ip" -p tcp --destination-port "$mitm_port" -j ACCEPT
+sudo iptables -D INPUT -d "$host_ip" -p tcp --destination-port "$mitm_port" -j DROP
+sudo bash recycle_honeypot_aux.sh "$container"
