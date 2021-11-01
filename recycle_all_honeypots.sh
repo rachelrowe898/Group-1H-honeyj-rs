7c803c8191ff75aaf75eca94942024538af76b8a
yieb <52669318+yieb@users.noreply.github.com>
2021-11-01 16:31:16 -0400

    Update recycle_all_honeypots.sh

--- recycle_all_honeypots.sh
+++ recycle_all_honeypots.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Run script on restart using crontab
 
-sudo bash recycle_honeypot.sh HRServeA 128.8.238.119 26 10000 /home/student/active_data_A >> /home/student/honeypot_logs/A_lifecycle.log
-sudo bash recycle_honeypot.sh HRServeB 128.8.238.86 26 10001 /home/student/active_data_B >> /home/student/honeypot_logs/B_lifecycle.log
-sudo bash recycle_honeypot.sh HRServeC 128.8.238.120 26 10002 /home/student/active_data_C >> /home/student/honeypot_logs/C_lifecycle.log
-sudo bash recycle_honeypot.sh HRServeD 128.8.37.125 27 10003 /home/student/active_data_D >> /home/student/honeypot_logs/D_lifecycle.log
+sudo bash recycle_honeypot_aux.sh A 1
+sudo bash recycle_honeypot_aux.sh B 1
+sudo bash recycle_honeypot_aux.sh C 1
+sudo bash recycle_honeypot_aux.sh D 1
