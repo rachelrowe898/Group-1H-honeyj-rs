dbe7fafbdbf14596823907de8541be6bb15ba7f4
yieb <52669318+yieb@users.noreply.github.com>
2021-11-01 17:15:47 -0400

    Add optional flag to run without data compression

--- recycle_all_honeypots.sh
+++ recycle_all_honeypots.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 # Run script on restart using crontab
 
-sudo bash recycle_honeypot_aux.sh A 1
-sudo bash recycle_honeypot_aux.sh B 1
-sudo bash recycle_honeypot_aux.sh C 1
-sudo bash recycle_honeypot_aux.sh D 1
+if [ $# -gt 1 ]; then
+  echo "usage: $0 <optional: compress_data_flag>"
+  exit 1
+fi
+
+# default is to compress data after recycling
+compress_data_flag=${1:-1}
+
+if [ "$compress_data_flag" != "1" ] && [ "$compress_data_flag" != "0" ]; then
+  echo "Invalid argument for <compress_data_flag>: $compress_data_flag"
+  exit 1
+fi
+
+sudo bash recycle_honeypot_aux.sh A "$compress_data_flag"
+sudo bash recycle_honeypot_aux.sh B "$compress_data_flag"
+sudo bash recycle_honeypot_aux.sh C "$compress_data_flag"
+sudo bash recycle_honeypot_aux.sh D "$compress_data_flag"
