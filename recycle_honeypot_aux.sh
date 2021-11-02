a603a50286f4ab163b3020b1d804b0b88abd2ee0
yieb <52669318+yieb@users.noreply.github.com>
2021-11-02 16:47:49 -0400

    Redirect stderr to lifecycle logs

--- recycle_honeypot_aux.sh
+++ recycle_honeypot_aux.sh
@@ -11,13 +11,13 @@ fi
 honeypot=$(echo "$1" | sed 's/HRServe//')
 
 if [ "$honeypot" == "A" ]; then
-  sudo bash recycle_honeypot.sh HRServeA 128.8.238.119 26 10000 /home/student/active_data_A "$2" >> /home/student/honeypot_logs/A_lifecycle.log
+  sudo bash recycle_honeypot.sh HRServeA 128.8.238.119 26 10000 /home/student/active_data_A "$2" >> /home/student/honeypot_logs/A_lifecycle.log 2>&1
 elif [ "$honeypot" == "B" ]; then 
-  sudo bash recycle_honeypot.sh HRServeB 128.8.238.86 26 10001 /home/student/active_data_B "$2" >> /home/student/honeypot_logs/B_lifecycle.log
+  sudo bash recycle_honeypot.sh HRServeB 128.8.238.86 26 10001 /home/student/active_data_B "$2" >> /home/student/honeypot_logs/B_lifecycle.log 2>&1
 elif [ "$honeypot" == "C" ]; then 
-  sudo bash recycle_honeypot.sh HRServeC 128.8.238.120 26 10002 /home/student/active_data_C "$2" >> /home/student/honeypot_logs/C_lifecycle.log
+  sudo bash recycle_honeypot.sh HRServeC 128.8.238.120 26 10002 /home/student/active_data_C "$2" >> /home/student/honeypot_logs/C_lifecycle.log 2>&1
 elif [ "$honeypot" == "D" ]; then 
-  sudo bash recycle_honeypot.sh HRServeD 128.8.37.125 27 10003 /home/student/active_data_D "$2" >> /home/student/honeypot_logs/D_lifecycle.log
+  sudo bash recycle_honeypot.sh HRServeD 128.8.37.125 27 10003 /home/student/active_data_D "$2" >> /home/student/honeypot_logs/D_lifecycle.log 2>&1
 else
   echo "Invalid honeypot name. Cannot recycle honeypot."
   exit 1
