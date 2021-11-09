22ec4759f6a3f1ea8041c4028cfdf4f9849e6c4e
stcwang <73036278+stcwang@users.noreply.github.com>
2021-11-09 01:02:48 -0500

    Update data_compression.sh

--- data_compression.sh
+++ data_compression.sh
@@ -5,21 +5,24 @@
 # else where
 
 
-if [ $# -ne 4 ]
+if [ $# -ne 5 ]
 then
   echo "Usage: $0
       <file path to directory with />
       <compressed files dest path with />
       <honeypot code>
-      <attackerID>"
+      <attackerID>
+      <mitm port - 10000>"
   exit 1
 fi
+
+
 
 if [ -d "$1" ]
 then
   zipname="$3_$4$(date +_%m_%d)" #Zipped directory naming: [containerID]_[attackerID]_MM_DD.zip
   cd "/home/student"
-  localPath="active_data_$3"
+  localPath="active_data_$5"
   zip -r "$zipname" "$localPath"
   mv "$zipname.zip" "$2"
 
