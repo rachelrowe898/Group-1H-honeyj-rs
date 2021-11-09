8fea0b33187338f2576376b6f2e2a47a25c31e03
yieb <52669318+yieb@users.noreply.github.com>
2021-11-09 01:09:41 -0500

    Update delete_all_honeypots.sh

--- delete_all_honeypots.sh
+++ delete_all_honeypots.sh
@@ -3,7 +3,7 @@
 
 mitm_path="/home/student/Group-1H-honeyj-rs/MITM/mitm/index.js"
 
-sudo bash delete_honeypot.sh HRServeA 128.8.238.119 26 10000 "$mitm_path" 0
-sudo bash delete_honeypot.sh HRServeB 128.8.238.86 26 10001 "$mitm_path" 0
-sudo bash delete_honeypot.sh HRServeC 128.8.238.120 26 10002 "$mitm_path" 0
-sudo bash delete_honeypot.sh HRServeD 128.8.37.125 27 10003 "$mitm_path" 0
+sudo bash delete_honeypot.sh "$(sudo lxc-ls -1 | grep 0)" 128.8.238.119 26 10000 "$mitm_path" 0
+sudo bash delete_honeypot.sh "$(sudo lxc-ls -1 | grep 1)" 128.8.238.86 26 10001 "$mitm_path" 0
+sudo bash delete_honeypot.sh "$(sudo lxc-ls -1 | grep 2)" 128.8.238.120 26 10002 "$mitm_path" 0
+sudo bash delete_honeypot.sh "$(sudo lxc-ls -1 | grep 3)" 128.8.37.125 27 10003 "$mitm_path" 0
