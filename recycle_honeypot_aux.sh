251ff3ca48bff84569e6ab3d1e1379909efa6dbb
yieb <52669318+yieb@users.noreply.github.com>
2021-11-08 23:48:04 -0500

    Add new arguments to account for randomization

--- recycle_honeypot_aux.sh
+++ recycle_honeypot_aux.sh
@@ -4,13 +4,16 @@
 
 # First check if proper number of shell arguments is given
 if [ $# -ne 2 ]; then
-  echo "usage: $0 <honeypot_name> <optional: compress_data_flag>"
+  echo "usage: $0 <honeypot_name> <mitm_port> <optional: compress_data_flag>"
   exit 1
 fi
 
+honeypot=$1
+mitm_port=$2
+
 # compress data only if compress_data_flag is set to 1
 # default is to compress data after recycling
-compress_data_flag=${2:-1}
+compress_data_flag=${3:-1}
 
 if [ "$compress_data_flag" != "1" ] && [ "$compress_data_flag" != "0" ]; then
   echo "Invalid argument for <compress_data_flag>: $compress_data_flag"
@@ -18,17 +21,15 @@ if [ "$compress_data_flag" != "1" ] && [ "$compress_data_flag" != "0" ]; then
   exit 1
 fi
 
-honeypot=$(echo "$1" | sed 's/HRServe//')
-
-if [ "$honeypot" == "A" ]; then
-  sudo bash recycle_honeypot.sh HRServeA 128.8.238.119 26 10000 /home/student/active_data_A "$2" >> /home/student/honeypot_logs/A_lifecycle.log 2>&1
-elif [ "$honeypot" == "B" ]; then 
-  sudo bash recycle_honeypot.sh HRServeB 128.8.238.86 26 10001 /home/student/active_data_B "$2" >> /home/student/honeypot_logs/B_lifecycle.log 2>&1
-elif [ "$honeypot" == "C" ]; then 
-  sudo bash recycle_honeypot.sh HRServeC 128.8.238.120 26 10002 /home/student/active_data_C "$2" >> /home/student/honeypot_logs/C_lifecycle.log 2>&1
-elif [ "$honeypot" == "D" ]; then 
-  sudo bash recycle_honeypot.sh HRServeD 128.8.37.125 27 10003 /home/student/active_data_D "$2" >> /home/student/honeypot_logs/D_lifecycle.log 2>&1
+if [ "$mitm_port" == 10000 ]; then
+  sudo bash recycle_honeypot.sh "$honeypot" 128.8.238.119 26 10000 /home/student/active_data_0 "$mitm_port" >> /home/student/honeypot_logs/0_lifecycle.log 2>&1
+elif [ "$mitm_port" == 10001 ]; then 
+  sudo bash recycle_honeypot.sh "$honeypot" 128.8.238.86 26 10001 /home/student/active_data_1 "$mitm_port" >> /home/student/honeypot_logs/1_lifecycle.log 2>&1
+elif [ "$mitm_port" == 10002 ]; then 
+  sudo bash recycle_honeypot.sh "$honeypot" 128.8.238.120 26 10002 /home/student/active_data_2 "$mitm_port" >> /home/student/honeypot_logs/2_lifecycle.log 2>&1
+elif [ "$mitm_port" == 10003 ]; then 
+  sudo bash recycle_honeypot.sh "$honeypot" 128.8.37.125 27 10003 /home/student/active_data_3 "$mitm_port" >> /home/student/honeypot_logs/3_lifecycle.log 2>&1
 else
-  echo "Invalid honeypot name. Cannot recycle honeypot. Please input either A, B, C, or D."
+  echo "Invalid MITM Port. Cannot recycle honeypot. Please input either 10000, 10001, 10002, 10003."
   exit 1
 fi
