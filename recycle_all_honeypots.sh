7961cc88aedd8330e13c02b76e732e77e4b7677d
stcwang <73036278+stcwang@users.noreply.github.com>
2021-11-09 17:50:57 -0500

    Update recycle_all_honeypots.sh (#60)

--- recycle_all_honeypots.sh
+++ recycle_all_honeypots.sh
@@ -15,7 +15,7 @@ if [ "$compress_data_flag" != "1" ] && [ "$compress_data_flag" != "0" ]; then
   exit 1
 fi
 
-sudo bash recycle_honeypot_aux.sh $(sudo lxc-ls -1 | grep 0 || "NO_CONTAINER") 10000 "$compress_data_flag"
-sudo bash recycle_honeypot_aux.sh $(sudo lxc-ls -1 | grep 1 || "NO_CONTAINER") 10001 "$compress_data_flag"
-sudo bash recycle_honeypot_aux.sh $(sudo lxc-ls -1 | grep 2 || "NO_CONTAINER") 10002 "$compress_data_flag"
-sudo bash recycle_honeypot_aux.sh $(sudo lxc-ls -1 | grep 3 || "NO_CONTAINER") 10003 "$compress_data_flag"
+sudo bash recycle_honeypot_aux.sh $(sudo lxc-ls -1 | grep 0 || echo "NO_CONTAINER") 10000 "$compress_data_flag"
+sudo bash recycle_honeypot_aux.sh $(sudo lxc-ls -1 | grep 1 || echo "NO_CONTAINER") 10001 "$compress_data_flag"
+sudo bash recycle_honeypot_aux.sh $(sudo lxc-ls -1 | grep 2 || echo "NO_CONTAINER") 10002 "$compress_data_flag"
+sudo bash recycle_honeypot_aux.sh $(sudo lxc-ls -1 | grep 3 || echo "NO_CONTAINER") 10003 "$compress_data_flag"
