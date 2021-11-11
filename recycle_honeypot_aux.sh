f1c557642bcb73bbf27fd294e546871d2a3714a4
yieb <52669318+yieb@users.noreply.github.com>
2021-11-11 15:59:32 -0500

    Fix arguments passed into recycle_honeypot.sh

--- recycle_honeypot_aux.sh
+++ recycle_honeypot_aux.sh
@@ -22,13 +22,13 @@ if [ "$compress_data_flag" != "1" ] && [ "$compress_data_flag" != "0" ]; then
 fi
 
 if [ "$mitm_port" == "10000" ]; then
-  sudo bash recycle_honeypot.sh "$honeypot" 128.8.238.119 26 10000 /home/student/active_data_0 "$mitm_port" >> /home/student/honeypot_logs/0_lifecycle.log 2>&1
+  sudo bash recycle_honeypot.sh "$honeypot" 128.8.238.119 26 10000 /home/student/active_data_0 "$compress_data_flag" >> /home/student/honeypot_logs/0_lifecycle.log 2>&1
 elif [ "$mitm_port" == "10001" ]; then 
-  sudo bash recycle_honeypot.sh "$honeypot" 128.8.238.86 26 10001 /home/student/active_data_1 "$mitm_port" >> /home/student/honeypot_logs/1_lifecycle.log 2>&1
+  sudo bash recycle_honeypot.sh "$honeypot" 128.8.238.86 26 10001 /home/student/active_data_1 "$compress_data_flag" >> /home/student/honeypot_logs/1_lifecycle.log 2>&1
 elif [ "$mitm_port" == "10002" ]; then 
-  sudo bash recycle_honeypot.sh "$honeypot" 128.8.238.120 26 10002 /home/student/active_data_2 "$mitm_port" >> /home/student/honeypot_logs/2_lifecycle.log 2>&1
+  sudo bash recycle_honeypot.sh "$honeypot" 128.8.238.120 26 10002 /home/student/active_data_2 "$compress_data_flag" >> /home/student/honeypot_logs/2_lifecycle.log 2>&1
 elif [ "$mitm_port" == "10003" ]; then 
-  sudo bash recycle_honeypot.sh "$honeypot" 128.8.37.125 27 10003 /home/student/active_data_3 "$mitm_port" >> /home/student/honeypot_logs/3_lifecycle.log 2>&1
+  sudo bash recycle_honeypot.sh "$honeypot" 128.8.37.125 27 10003 /home/student/active_data_3 "$compress_data_flag" >> /home/student/honeypot_logs/3_lifecycle.log 2>&1
 else
   echo "Invalid MITM Port. Cannot recycle honeypot. Please input either 10000, 10001, 10002, 10003."
   exit 1
